e9181063f7636302e59bdc54a314199bae472157
loweffort-alt <farfan_alexander@outlook.com>
2023-07-26 05:56:49 -0500

    Validations added to my backend

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,12 +7,14 @@ import {
   deleteProfile,
 } from "../controllers/auth.controllers.js";
 import { authRequired } from "../middlewares/validateToken.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import { registerSchema, loginSchema } from "../schemas/auth.schema.js";
 
 const router = Router();
 
-router.post("/register", register);
+router.post("/register", validateSchema(registerSchema), register);
 
-router.post("/login", login);
+router.post("/login", validateSchema(loginSchema), login);
 
 router.post("/logout", authRequired, logout);
 
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -7,11 +7,18 @@ import {
   updateTask,
   deleteTask,
 } from "../controllers/task.controllers.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import { createTaskSchema } from "../schemas/task.schema.js";
 
 const router = Router();
 
 router.get("/tasks", authRequired, getTasks);
-router.post("/tasks", authRequired, createTask);
+router.post(
+  "/tasks",
+  authRequired,
+  validateSchema(createTaskSchema),
+  createTask
+);
 router.get("/tasks/:id", authRequired, getTask);
 router.delete("/tasks/:id", authRequired, deleteTask);
 router.put("/tasks/:id", authRequired, updateTask);
