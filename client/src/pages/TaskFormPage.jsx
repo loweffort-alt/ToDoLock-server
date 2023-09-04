c2a2fad841eb4fe1cbc0391e9e5680012934c481
loweffort-alt <farfan_alexander@outlook.com>
2023-09-04 15:41:14 -0500

    New Feature: TaskFormPage and TaskContext

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,5 +1,44 @@
+import { useForm } from "react-hook-form";
+
 const TaskFormPage = () => {
-  return <h1>Hola Amiguitos</h1>;
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
+  return (
+    <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+      <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md text-center">
+        <h1 className="text-2xl font-bold text-center mb-4">Create new task</h1>
+        <form action="" onSubmit={onSubmit}>
+          <div className="flex-col gap-3">
+            <input
+              type="text"
+              placeholder="Title"
+              {...register("title")}
+              autoFocus
+              className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md"
+            />
+            <textarea
+              id=""
+              name=""
+              placeholder="Description"
+              {...register("description")}
+              className="w-full bg-zinc-700 text-white px-4 py-2 mt-2 rounded-md"
+              rows="3"
+            ></textarea>
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Post
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default TaskFormPage;
