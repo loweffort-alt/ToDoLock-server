a2cfae47e7b6cf639051c6bd04ceadf46438b761
loweffort-alt <farfan_alexander@outlook.com>
2024-03-04 02:52:27 -0500

    Hotfix: middleware response with Google recomendations

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import helmet from "helmet";
 import "dotenv/config.js";
 
 import authRoutes from "./routes/auth.routes.js";
@@ -9,12 +10,19 @@ import taskRoutes from "./routes/task.routes.js";
 
 const app = express();
 
+// Middleware para configurar el encabezado Cache-Control en todas las respuestas
+app.use((req, res, next) => {
+  res.setHeader("Cache-Control", "no-store"); // Establece el valor no-store para evitar el almacenamiento en caché
+  next();
+});
+
 app.use(
   cors({
     origin: process.env.LOCAL_CORS,
     credentials: true,
   }),
 );
+app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
